e96247869b33009fbade35a2fbe2b9b659a89e07
Stijn Callebaut <stijnc@fooji.net>
2016-07-08 21:36:56 +0200

    removed v4 specified items to get a valid build in VSTS in build.ps1

--- build.ps1
+++ build.ps1
@@ -1,41 +1,8 @@
-﻿
+﻿# Grab nuget bits, install modules, set build variables, start build.
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-if($PSVersionTable.PSVersion.Major -eq 5){
-    # Grab nuget bits, install modules, set build variables, start build.
-    Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-
-    Install-Module Psake, PSDeploy, Pester, BuildHelpers -force
-    Import-module Psake, PSDeploy, Pester, BuildHelpers
-}
-else {
-    $modulePath = Join-Path "$env:temp" "Pester-master\Pester.psm1"
-    if (-not(Test-Path $modulePath)) {
-        $tempFile = Join-Path $env:TEMP pester.zip
-        Invoke-WebRequest 'https://github.com/pester/Pester/archive/master.zip' -OutFile $tempFile -usebasicparsing
-        [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $env:temp)
-    }
-    Import-Module $modulePath -DisableNameChecking
-    $modulePath = Join-Path "$env:temp" "PSake-master\Psake.psm1"
-    if (-not(Test-Path $modulePath)) {
-        $tempFile = Join-Path $env:TEMP psake.zip;
-        Invoke-WebRequest 'https://github.com/psake/psake/archive/master.zip' -OutFile $tempFile -usebasicparsing
-        [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $env:temp)
-    }
-    Import-Module $modulePath -DisableNameChecking
-    $modulePath = Join-Path "$env:temp" "PSDeploy-master\PSDeploy\PSDeploy.psm1"
-    if (-not(Test-Path $modulePath)) {
-        $tempFile = Join-Path $env:TEMP psdeploy.zip;
-        Invoke-WebRequest 'https://github.com/RamblingCookieMonster/PSDeploy/archive/master.zip' -OutFile $tempFile -usebasicparsing
-        [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $env:temp)
-    }
-    Import-Module $modulePath -DisableNameChecking
-}
-
-
-Import-module .\BuildHelpers\BuildHelpers.psm1
+Install-Module Psake, PSDeploy, Pester, BuildHelpers -force
+Import-module Psake, PSDeploy, Pester, BuildHelpers
 
 Set-BuildEnvironment
 
