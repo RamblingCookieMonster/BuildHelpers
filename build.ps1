dbdc0e89b54f7f76cc859d0d797c1ba042affcea
Stijn Callebaut <stijnc@fooji.net>
2016-07-08 20:07:45 +0200

    added simple psversion 4 without packagemanagement logic

--- build.ps1
+++ build.ps1
@@ -1,8 +1,26 @@
-﻿# Grab nuget bits, install modules, set build variables, start build.
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+﻿
 
-Install-Module Psake, PSDeploy, Pester, BuildHelpers -force
-Import-Module Psake, BuildHelpers
+if($PSVersionTable.PSVersion.Major -eq 5){
+    # Grab nuget bits, install modules, set build variables, start build.
+    Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+    Install-Module Psake, PSDeploy, Pester, BuildHelpers -force
+}
+else {
+    $modulePath = Join-Path "$env:temp" "Pester-master\Pester.psm1"
+    if (-not(Test-Path $modulePath)) {
+        $tempFile = Join-Path $env:TEMP pester.zip;Invoke-WebRequest 'https://github.com/pester/Pester/archive/master.zip' -OutFile $tempFile -usebasicparsing
+        [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $env:temp)
+    }
+    $modulePath = Join-Path "$env:temp" "PSake-master\Psake.psm1"
+    if (-not(Test-Path $modulePath)) {
+        $tempFile = Join-Path $env:TEMP pester.zip;Invoke-WebRequest 'https://github.com/psake/psake/archive/master.zip' -OutFile $tempFile -usebasicparsing
+        [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($tempFile, $env:temp)
+    }
+}
+Import-Module Psake, Pester, BuildHelpers
 
 Set-BuildEnvironment
 
