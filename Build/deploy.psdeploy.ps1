5279aed379853f6fc8d7a0f6bd9032271d90c884
Warren Frame <ramblingcookiemonster@users.noreply.github.com>
2017-04-07 22:23:30 -0400

    Revert to BHPSModulePath for deployments
    * Need to deploy, too lazy to do it manually, and version out there
    doesn't create BHModulePath, so no deployment
    * !Deploy

--- Build/deploy.psdeploy.ps1
+++ Build/deploy.psdeploy.ps1
@@ -19,7 +19,7 @@
 
 # Publish to gallery with a few restrictions
 if(
-    $env:BHModulePath -and
+    $env:BHPSModulePath -and
     $env:BHBuildSystem -ne 'Unknown' -and
     $env:BHBranchName -eq "master" -and
     $env:BHCommitMessage -match '!deploy'
@@ -27,7 +27,7 @@ if(
 {
     Deploy Module {
         By PSGalleryModule {
-            FromSource $ENV:BHModulePath
+            FromSource $ENV:BHPSModulePath
             To PSGallery
             WithOptions @{
                 ApiKey = $ENV:NugetApiKey
@@ -46,13 +46,13 @@ else
 
 # Publish to AppVeyor if we're in AppVeyor
 if(
-    $env:BHModulePath -and
+    $env:BHPSModulePath -and
     $env:BHBuildSystem -eq 'AppVeyor'
    )
 {
     Deploy DeveloperBuild {
         By AppVeyorModule {
-            FromSource $ENV:BHModulePath
+            FromSource $ENV:BHPSModulePath
             To AppVeyor
             WithOptions @{
                 Version = $env:APPVEYOR_BUILD_VERSION
