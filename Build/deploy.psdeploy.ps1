846b1b95057ec633703221db9d69b3ad0efa0d1d
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2017-12-08 06:59:49 -0500

    Add more verbosity to deploy gate and !deploy

--- Build/deploy.psdeploy.ps1
+++ Build/deploy.psdeploy.ps1
@@ -38,10 +38,10 @@ if(
 else
 {
     "Skipping deployment: To deploy, ensure that...`n" +
-    "`t* ENV:BHModulePath is set (Current: $ENV:BHPSModulePath)`n" +
-    "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
-    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
-    "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
+    "`t* ENV:BHModulePath is set (Current [$([bool]$env:BHModulePath)]: $ENV:BHPSModulePath)`n" +
+    "`t* You are in a known build system (Current [$($env:BHBuildSystem -ne 'Unknown')]: $ENV:BHBuildSystem)`n" +
+    "`t* You are committing to the master branch (Current [$($env:BHBranchName -eq "master")]: $ENV:BHBranchName) `n" +
+    "`t* Your commit message includes !deploy (Current [$($env:BHCommitMessage -match '!deploy')]: $ENV:BHCommitMessage)" |
         Write-Host
 }
 
