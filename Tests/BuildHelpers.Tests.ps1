2441ad8e737a03aab8f3c78f9ea01ec47f585771
markwragg <mark.wragg@gmail.com>
2018-05-14 15:16:21 +0100

    Updated Pester test as CI pipeline is using old Pester version

--- Tests/BuildHelpers.Tests.ps1
+++ Tests/BuildHelpers.Tests.ps1
@@ -321,7 +321,7 @@ Describe 'Set-CodeCoverageBadge' {
         Set-CodeCoverageBadge -TextFilePath TestDrive:\readme.md -CodeCoverage 75
 
         It 'Should update the dummy readme.md with code coverage' {
-            Get-Content TestDrive:\readme.md | Should -be '![Test Coverage](https://img.shields.io/badge/coverage-75%25-yellow.svg)'
+            Get-Content TestDrive:\readme.md | Should Be '![Test Coverage](https://img.shields.io/badge/coverage-75%25-yellow.svg)'
         }
     }
 }
