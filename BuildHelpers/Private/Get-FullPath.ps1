bf7ca11458bcb3c0f4ab2179743de9d361064845
Oliver Lipkau <oliver@lipkau.net>
2018-07-10 20:12:14 +0200

    Added Comment-Based Help to function

--- BuildHelpers/Private/Get-FullPath.ps1
+++ BuildHelpers/Private/Get-FullPath.ps1
@@ -1,14 +1,50 @@
-function Get-FullPath ([string]$Path) {
-    # https://github.com/pester/Pester/blob/5796c95e4d6ff5528b8e14865e3f25e40f01bd65/Functions/TestResults.ps1#L13-L27
-    $Folder = Split-Path -Path $Path -Parent
-    $File = Split-Path -Path $Path -Leaf
-    if ( -not ([String]::IsNullOrEmpty($Folder))) {
-        $FolderResolved = Resolve-Path -Path $Folder
-    }
-    else {
-        $FolderResolved = Resolve-Path -Path $ExecutionContext.SessionState.Path.CurrentFileSystemLocation
-    }
-    $Path = Join-Path -Path $FolderResolved.ProviderPath -ChildPath $File
+function Get-FullPath {
+    <#
+    .SYNOPSIS
+        Get the full qualified path of a file or folder
+    .DESCRIPTION
+        Get the full qualified path of a file or folder
 
-    return $Path
+        Native cmdlets do not resolve relative paths and paths containing a
+        PSDrive properly.
+        This function is able to resolve both of these into full qualified
+        paths.
+    .EXAMPLE
+        PS C:\project\MyProject> Get-FullPath ./.gitignore
+
+        Returns the full path of .gitignore:
+        C:\project\MyProject\.gitignore
+    .EXAMPLE
+        PS Projects:\MyProject>  Get-FullPath ./.gitignore
+
+        Returns the full path of .gitignore:
+        C:\project\MyProject\.gitignore
+    .LINK
+        https://github.com/pester/Pester/blob/5796c95e4d6ff5528b8e14865e3f25e40f01bd65/Functions/TestResults.ps1#L13-L27
+    #>
+    [CmdletBinding()]
+    param(
+        # A Path to a file or folder
+        #
+        # Can be a full path or relative to the current working directory
+        [Parameter( Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName )]
+        [Alias("FullName", "PSPath")]
+        [String[]]$Path
+    )
+
+    process {
+        foreach ($_path in $Path) {
+            $folder = Split-Path -Path $_path -Parent
+            $file = Split-Path -Path $_path -Leaf
+
+            if ( -not ([String]::IsNullOrEmpty($folder))) {
+                $FolderResolved = Resolve-Path -Path $folder
+            }
+            else {
+                $folderResolved = Resolve-Path -Path $ExecutionContext.SessionState.Path.CurrentFileSystemLocation
+            }
+
+            Join-Path -Path $folderResolved.ProviderPath -ChildPath $file
+        }
+    }
 }
