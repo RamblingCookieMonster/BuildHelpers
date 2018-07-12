75924ccc3745815bb40a70dde20b251c6e5e9f0a
Oliver Lipkau <oliver@lipkau.net>
2018-07-12 09:28:15 +0200

    Improved loading of Configuration module
    `Configuration` module which is shipped in this repository is now only
    loaded when no newer version of `Configuration` could be found on the system

--- BuildHelpers/BuildHelpers.psm1
+++ BuildHelpers/BuildHelpers.psm1
@@ -1,25 +1,37 @@
 #Get public and private function definition files.
-    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-    $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-    $ModuleRoot = $PSScriptRoot
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+$ModuleRoot = $PSScriptRoot
 
 #Dot source the files
-    Foreach($import in @($Public + $Private))
+Foreach($import in @($Public + $Private))
+{
+    Try
     {
-        Try
-        {
-            . $import.fullname
-        }
-        Catch
-        {
-            Write-Error -Message "Failed to import function $($import.fullname): $_"
-        }
+        . $import.fullname
     }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
 
 # Load dependencies. TODO: Move to module dependency once the bug that
 # causes this is fixed: https://ci.appveyor.com/project/RamblingCookieMonster/buildhelpers/build/1.0.22
 # Thanks to Joel Bennett for this!
-    Import-Module $PSScriptRoot\Private\Modules\Configuration
+$fallbackModule = Get-Module -Name $PSScriptRoot\Private\Modules\Configuration -ListAvailable
+if ($configModule = Get-Module $fallbackModule.Name -ListAvailable)
+{
+    $configModule |
+        Where-Object { $_.Version -gt $fallbackModule.Version} |
+        Sort-Object -Property Version -Descending |
+        Select-Object -First 1 |
+        Import-Module -Force
+}
+if (-not (Get-Module $fallbackModule.Name | Where-Object { $_.Version -gt $fallbackModule.Version}))
+{
+    $fallbackModule | Import-Module -Force
+}
 
 Export-ModuleMember -Function $Public.Basename
 Export-ModuleMember -Function Get-Metadata, Update-Metadata, Export-Metadata
