5dccf38ade78374a1493618c047e89a6888dca9e
Jeff Davis <14242442+DropBearHunta@users.noreply.github.com>
2018-10-26 10:49:55 -0500

    fix(Set-ModuleFormats): Fix Exception Checking

--- BuildHelpers/Public/Set-ModuleFormats.ps1
+++ BuildHelpers/Public/Set-ModuleFormats.ps1
@@ -73,7 +73,7 @@ function Set-ModuleFormats {
             }
             catch
             {
-                if ($PsItem -is [VMware.VimAutomation.ViCore.Cmdlets.Provider.Exceptions.DriveException])
+                if ($PsItem.Exception.GetType().FullName -eq 'VMware.VimAutomation.ViCore.Cmdlets.Provider.Exceptions.DriveException')
                 {
                     Import-Module -Name $Name -PassThru:$Passthru -Force:$Force
                 }
